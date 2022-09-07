27e0aecdd04b940a9e4daf55eb4d83fd00499c7f
Bayemite <bayemite@gmail.com>
2022-09-07 19:25:37 +1000

    reformat

--- app.cpp
+++ app.cpp
@@ -22,81 +22,81 @@ App::App() : window{sf::VideoMode({800, 600}), "Gravity", sf::Style::Default, sf
 }
 
 void App::handleEvents()
+{
+    sf::Event event;
+    while (window.pollEvent(event))
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        switch (event.type)
         {
-            switch (event.type)
+        case sf::Event::KeyPressed:
+        {
+            static sf::Vector2i cart_coords;
+            auto &data{event.key};
+            switch (data.code)
             {
-            case sf::Event::KeyPressed:
-            {
-                static sf::Vector2i cart_coords;
-                auto &data{event.key};
-                switch (data.code)
-                {
-                default:
-                    break;
-                }
-                break;
-            }
-            case sf::Event::MouseButtonPressed:
-            {
-                auto &data{event.mouseButton};
-                mousePixelCoords = sf::Vector2i(data.x, data.y);
-                mouseCoords = window.mapPixelToCoords(sf::Vector2i(data.x, data.y), view);
-                if (data.button == sf::Mouse::Left)
-                    leftClicked = true;
-                break;
-            }
-            case sf::Event::MouseButtonReleased:
-            {
-                auto &data{event.mouseButton};
-                if (data.button == sf::Mouse::Left)
-                    leftClicked = false;
-                break;
-            }
-            case sf::Event::MouseMoved:
-            {
-                auto &data{event.mouseMove};
-                sf::Vector2f coords{window.mapPixelToCoords(sf::Vector2i(data.x, data.y), view)};
-                if (leftClicked)
-                {
-                    sf::Vector2f offset{mousePixelCoords - sf::Vector2i(data.x, data.y)};
-                    view.move(offset * viewZoom);
-                    window.setView(view);
-                }
-                mousePixelCoords = sf::Vector2i(data.x, data.y);
-                mouseCoords = coords;
-                break;
-            }
-            case sf::Event::MouseWheelScrolled:
-            {
-                auto &data{event.mouseWheelScroll};
-                float factor = 1 + (-data.delta / 3);
-                viewZoom *= factor;
-                zoomViewAt({event.mouseWheelScroll.x, event.mouseWheelScroll.y}, window, factor, view);
-                break;
-            }
-            case sf::Event::Resized:
-            {
-                auto &data{event.size};
-                view.setSize(sf::Vector2f(data.width, data.height));
-                view.reset({{0.f, 0.f}, sf::Vector2f(data.width, data.height)});
-                window.setView(view);
-                break;
-            }
-            case sf::Event::Closed:
-            {
-                window.close();
-                break;
-            }
             default:
-            {
                 break;
             }
+            break;
+        }
+        case sf::Event::MouseButtonPressed:
+        {
+            auto &data{event.mouseButton};
+            mousePixelCoords = sf::Vector2i(data.x, data.y);
+            mouseCoords = window.mapPixelToCoords(sf::Vector2i(data.x, data.y), view);
+            if (data.button == sf::Mouse::Left)
+                leftClicked = true;
+            break;
+        }
+        case sf::Event::MouseButtonReleased:
+        {
+            auto &data{event.mouseButton};
+            if (data.button == sf::Mouse::Left)
+                leftClicked = false;
+            break;
+        }
+        case sf::Event::MouseMoved:
+        {
+            auto &data{event.mouseMove};
+            sf::Vector2f coords{window.mapPixelToCoords(sf::Vector2i(data.x, data.y), view)};
+            if (leftClicked)
+            {
+                sf::Vector2f offset{mousePixelCoords - sf::Vector2i(data.x, data.y)};
+                view.move(offset * viewZoom);
+                window.setView(view);
             }
+            mousePixelCoords = sf::Vector2i(data.x, data.y);
+            mouseCoords = coords;
+            break;
+        }
+        case sf::Event::MouseWheelScrolled:
+        {
+            auto &data{event.mouseWheelScroll};
+            float factor = 1 + (-data.delta / 3);
+            viewZoom *= factor;
+            zoomViewAt({event.mouseWheelScroll.x, event.mouseWheelScroll.y}, window, factor, view);
+            break;
+        }
+        case sf::Event::Resized:
+        {
+            auto &data{event.size};
+            view.setSize(sf::Vector2f(data.width, data.height));
+            view.reset({{0.f, 0.f}, sf::Vector2f(data.width, data.height)});
+            window.setView(view);
+            break;
+        }
+        case sf::Event::Closed:
+        {
+            window.close();
+            break;
+        }
+        default:
+        {
+            break;
+        }
         }
     }
+}
 
 void App::stepSim()
 {
@@ -129,7 +129,7 @@ void App::run()
         {
             auto &dot = paths[i];
             sf::Color color = dot.getFillColor();
-            color.a=i;
+            color.a = i;
             dot.setFillColor(color);
             window.draw(paths[i]);
         }
